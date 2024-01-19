4a6f09a836138aa4dfe79406af2fdfa2a9757a63
Simone La Bella <149832552+simolb7@users.noreply.github.com>
2024-01-19 17:55:29 +0100

    Update main.js

--- webui/src/main.js
+++ webui/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import axios from './services/axios.js';
 import ErrorMsg from './components/ErrorMsg.vue'
-import LoadingSpinner from './components/LoadingSpinner.vue'
 
 import './assets/dashboard.css'
 import './assets/main.css'
@@ -11,6 +10,5 @@ import './assets/main.css'
 const app = createApp(App)
 app.config.globalProperties.$axios = axios;
 app.component("ErrorMsg", ErrorMsg);
-app.component("LoadingSpinner", LoadingSpinner);
 app.use(router)
 app.mount('#app')
